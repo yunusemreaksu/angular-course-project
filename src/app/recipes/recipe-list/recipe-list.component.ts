c8cb12085fdc1b5108c533e2ebf6fcbc7195f66a
Yunus Emre Aksu <emre_aksu@live.com>
2023-01-06 09:57:25 +0300

    Created recipe model & added content to the recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://unsplash.com/photos/hwy3W3qFjgM'
+    ),
+  ];
 }
